6352a23b091ecfe19ab2792f81bce0786431b196
Syahuda Nahatmasuni <syahudanahatmasuni@gmail.com>
2023-10-28 23:58:37 +0700

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <ProductDisplay />
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
+import ProductDisplay from "./components/ProductDisplay.vue";
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    ProductDisplay
+  }
+};
+</script>
